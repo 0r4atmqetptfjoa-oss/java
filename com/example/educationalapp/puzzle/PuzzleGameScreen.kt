7074fdf6a05c58bbff0d18db44d8a79f96f863ab
0r4atmqetptfjoa-oss <0r4atmqetptfjoa@gmail.com>
2025-12-21 22:28:05 +0200

    asd

--- com/example/educationalapp/puzzle/PuzzleGameScreen.kt
+++ com/example/educationalapp/puzzle/PuzzleGameScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.input.pointer.consumeAllChanges
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
